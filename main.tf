2dfe1931bfd191cd5e28bb590471860d60575ed8
brbe571 <70946335+brbe571@users.noreply.github.com>
2022-12-28 13:54:36 +1300

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,68 +1,24 @@
-provider "aws" {
-    region = "${var.region}"
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
 }
 
-resource "aws_security_group" "web-server" {
+provider "docker" {}
 
-    name        = "web-server"
-    description = "Allow incoming HTTP Connections"
-    
-    ingress {
-
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-
-   }
-
-    egress {
-
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-
-    }
-
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
 }
 
-resource "aws_instance" "web-server" {
-
-    ami = "ami-02e136e904f3da870"
-
-    instance_type = "t2.micro"
-
-    key_name = "whizlabs-key"
-
-    security_groups = ["${aws_security_group.web-server.name}"]
-
- 
-    user_data = <<-EOF
-
-    #!/bin/bash
-
-    sudo su
-
-    yum update -y
-
-    yum install httpd -y
-
-    systemctl start httpd
-
-    systemctl enable httpd
-
-    echo "<html><h1> Welcome to Whizlabs. Happy Learning... </h1></html>" >> /var/www/html/index.html
-
-    EOF
-
- 
-    tags = {
-
-        Name = "web_instance"
-
-    }
-
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
 }
